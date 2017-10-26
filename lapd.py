6370c3e31abda0bf545acab2e0daeb63f9830bed
Cyrus Sethna <csethna@gmail.com>
2017-10-26 16:56:21 -0500

    augment data
    augment data with `day_of_week` index & create pivot table to build grid of weekly data.

--- lapd.py
+++ lapd.py
@@ -14,3 +14,16 @@ query = ("https://data.lacity.org/resource/mgue-vbsx.json?"
     "&$select=date_trunc_ymd(dispatch_date)%20AS%20date%2C%20count(*)"
     "&$order=date")
 raw_data = pd.read_json(query)
+
+# Augment the data frame with the day of the week and the start of the week that it's in.
+# This will make more sense soon...
+raw_data['day_of_week'] = [date.dayofweek for date in raw_data["date"]]
+raw_data['week'] = [(date - datetime.timedelta(days=date.dayofweek)).strftime("%Y-%m-%d") for date in raw_data["date"]]
+
+# Pivot our data to get the matrix we need
+data = raw_data.pivot(index='week', columns='day_of_week', values='count')
+data = data.fillna(value=0)
+
+# Get our "weeks" and "days"
+weeks = list(data.index)
+days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
