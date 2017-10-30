0dbf8a8e6ea0b235c10715baf707443ddd838c1b
Cyrus Sethna <csethna@gmail.com>
2017-10-30 17:10:31 -0500

    ˆjson to pd dataframe

--- pandas-fundamentals/02/demos/demos/collection-master/demo_2.py
+++ pandas-fundamentals/02/demos/demos/collection-master/demo_2.py
@@ -9,4 +9,19 @@ pd.DataFrame.from_records(records)
 pd.DataFrame.from_records(records, columns=["Coffee", "Price"])
 
 #####
-KEYS_TO_USE = ['id', 'all_artists', 'title', 'medium', 'acquisitionYear', 'height', 'width']
+KEYS_TO_USE = ['id', 'all_artists', 'title', 'medium', 'dateText', 'acquisitionYear', 'height', 'width', 'units']
+
+def get_record_from_file(file_path, KEYS_TO_USE):
+    with open(file_path) as artwork_file:
+        content = json.load(artwork_file)
+
+    record = []
+    for field in KEYS_TO_USE:
+        record.append(content[field])
+
+    return tuple(record)
+
+# Single file processing function demo
+SAMPLE_JSON = os.path.join('..', 'collection-master', 'artworks', 'a', '000', 'a00001-1035.json')
+
+sample_record = get_record_from_file(SAMPLE_JSON, KEYS_TO_USE)
