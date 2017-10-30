ca6a06a484f022f23142416fbceb62e7054c5fb5
Cyrus Sethna <csethna@gmail.com>
2017-10-30 15:36:53 -0500

    specify an index

--- pandas-fundamentals/02/demos/demos/collection-master/demo_1.py
+++ pandas-fundamentals/02/demos/demos/collection-master/demo_1.py
@@ -7,4 +7,7 @@ CSV_PATH = os.path.join('..', 'collection-master', 'artwork_data.csv')
 # Read just 5 rows to see what's there
 df = pd.read_csv(CSV_PATH, nrows=5)
 
+# Specify an Index
+df = pd.read_csv(CSV_PATH, nrows=5, index_col='id')
+
 print(df)
