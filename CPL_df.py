813758026c89bd72508f5fc0d18ed87baf371818
Cyrus Sethna <csethna@gmail.com>
2017-10-31 16:06:30 -0500

    computer sessions DataFrame

--- CPL_df.py
+++ CPL_df.py
@@ -2,10 +2,16 @@
 import pandas as pd
 import os
 
+# Specify the data source
 CSV_PATH = os.path.join('Libraries_-_2017_Wi_Fi_Usage.csv')
-
+# Create the DataFrame
 df = pd.read_csv(CSV_PATH)
-
-print(df)
-
+# Save DataFrame to .pickle for later
 df.to_pickle(os.path.join('wifi_sessions.pickle'))
+
+# Import CPL computer sessions by location to DataFrame
+CSV_PATH = os.path.join('Libraries_-_2017_Computer_Sessions_by_Location.csv')
+# Create the DataFrame with 'LOCATION' as the index column
+df = pd.read_csv(CSV_PATH, index_col='LOCATION')
+# Save DataFrame to .pickle for later
+df.to_pickle(os.path.join('computer_sessions.pickle'))
